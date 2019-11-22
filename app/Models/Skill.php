cde1a8156757285a9f8da53ecf59f59ac1a8f237
ahmed-laravel-project <57094877+ahmed-laravel-project@users.noreply.github.com>
2019-11-22 20:14:30 +0100

    Create Skill.php

---
+++ app/Models/Skill.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+class Skill extends Model
+{
+    protected $fillable = ["name"];
+    public function videos(){
+        return $this->belongsToMany(Video::class , 'skills_videos');
+    }
+}
+© 2019 GitHub, Inc.
